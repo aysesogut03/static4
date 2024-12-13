faed42b61e0e230e40e54f864240ed189ef2760e
aysesogut03 <aysesogut2003@gmail.com>
2024-12-13 16:58:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            kullanici k1 = new kullanici(1, "Ayşe", "Söğüt");
+            k1.BilgileriGoster();
+            k1.zamYap(400);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ kullanici.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    public class kullanici
+    {
+        private int kullaniciİd{get;set;}
+        private string kullaniciAdi { get; set; }
+        private string kullaniciSoyadi { get; set; }
+        private static int kullanicimaas { get; set; }
+
+        static kullanici()
+            {
+            kullanicimaas = 79000;
+            }
+
+        public kullanici(int kullaniciİd, string kullaniciAdi, string kullaniciSoyadi)
+        {
+            this.kullaniciİd = kullaniciİd;
+            this.kullaniciAdi = kullaniciAdi;
+            this.kullaniciSoyadi = kullaniciSoyadi;
+        }
+
+        public void BilgileriGoster()
+        {
+            Console.WriteLine("Bilgileri goster:");
+            Console.WriteLine("kullaniciİd:" + kullaniciİd);
+            Console.WriteLine("kullaniciAdi:" + kullaniciAdi);
+            Console.WriteLine("kullaniciSoyadi:" + kullaniciSoyadi);
+            Console.WriteLine("kullanıcı maaşı:" + kullanicimaas);
+        }
+
+        public void zamYap(int eklenenzam)
+        {
+            Console.WriteLine("KULLANICIYA ZAM YAPILIYOR...");
+            Console.WriteLine("kullanıcının yeni maaş:"+ kullanicimaas + eklenenzam);
+
+        }
+    }
+}
